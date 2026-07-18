8bc0015902b88799d0fb3616c983b073770141b1
Vylyne <94922829+Vylyne@users.noreply.github.com>
2026-07-17 23:17:06 -0400

    FEAT: add tool # to config as optional and add tool # and mcu_name to status.

--- klippy_extras/buffer_manager.py
+++ klippy_extras/buffer_manager.py
@@ -13,13 +13,19 @@ MODE_AUTO = 0
 MODE_FORWARD = 1
 MODE_BACK = 2
 MODE_STOP = 3
-MODE_NAMES = {'AUTO': MODE_AUTO, 'FORWARD': MODE_FORWARD,
-              'BACK': MODE_BACK, 'STOP': MODE_STOP}
+MODE_NAMES = {
+    "AUTO": MODE_AUTO,
+    "FORWARD": MODE_FORWARD,
+    "BACK": MODE_BACK,
+    "STOP": MODE_STOP,
+}
 MODE_DIRECTION = {MODE_AUTO: 0, MODE_FORWARD: 1, MODE_BACK: -1, MODE_STOP: 0}
 
-KEEPALIVE_INTERVAL = 0.25   # seconds between re-armed sends while overridden
-MCU_TIMEOUT_MS = 1000       # firmware watchdog window per send - must be > KEEPALIVE_INTERVAL
-QUERY_INTERVAL = 0.25       # seconds between buffer_query_state polls
+KEEPALIVE_INTERVAL = 0.25  # seconds between re-armed sends while overridden
+MCU_TIMEOUT_MS = (
+    1000  # firmware watchdog window per send - must be > KEEPALIVE_INTERVAL
+)
+QUERY_INTERVAL = 0.25  # seconds between buffer_query_state polls
 
 
 class BufferManager:
@@ -28,57 +34,76 @@ class BufferManager:
 
         self.printer = config.get_printer()
         self.name = config.get_name().split()[-1]
-        self.feed_speed = config.getfloat('feed_speed_mm_s', above=0.)
-        mcu_name = config.get('mcu', self.name)
+        self.feed_speed = config.getfloat("feed_speed_mm_s", above=0.0)
+        self.mcu_name = config.get("mcu", self.name)
+        self.tool = config.getint('tool', -1)
 
-        self.mcu = self.printer.lookup_object('mcu ' + mcu_name)
+        self.mcu = self.printer.lookup_object("mcu " + self.mcu_name)
 
         # ------------- define variables ------------
-        self.stall_seconds = config.getfloat('stall_seconds', 5.0, above=0.)
+        self.stall_seconds = config.getfloat("stall_seconds", 5.0, above=0.0)
         self.hall_stuck_since = {1: None, 2: None, 3: None}
 
         self.host_mode = MODE_AUTO
         self.direction = 0
-        self.est_position = 0.
+        self.est_position = 0.0
         self.last_update_t = None
 
-        self.state = {'hall1': 0, 'hall2': 0, 'hall3': 0, 'fil': 0,
-                      'error': 0, 'motor_state': 0, 'host_mode': 0}
+        self.state = {
+            "hall1": 0,
+            "hall2": 0,
+            "hall3": 0,
+            "fil": 0,
+            "error": 0,
+            "motor_state": 0,
+            "host_mode": 0,
+        }
         self.last_state_t = None
 
         self.set_mode_cmd = None
         self.query_cmd = None
 
-        # ---- Register timers and reactor events ---- 
+        # ---- Register timers and reactor events ----
         self.reactor = self.printer.get_reactor()
         self.keepalive_timer = self.reactor.register_timer(
-            self._keepalive, self.reactor.NEVER)
+            self._keepalive, self.reactor.NEVER
+        )
         self.query_timer = self.reactor.register_timer(
-            self._do_query, self.reactor.NEVER)
+            self._do_query, self.reactor.NEVER
+        )
 
-        self.printer.register_event_handler('klippy:connect',
-                                             self._handle_connect)
-        self.printer.register_event_handler('klippy:disconnect',
-                                             self._handle_disconnect)
+        self.printer.register_event_handler("klippy:connect", self._handle_connect)
+        self.printer.register_event_handler(
+            "klippy:disconnect", self._handle_disconnect
+        )
 
         # ----------- Render gcode macros -----------
-        gcode = self.printer.lookup_object('gcode')
+        gcode = self.printer.lookup_object("gcode")
         gcode.register_mux_command(
-            'BUFFER_SET_MODE', 'BUFFER', self.name,
+            "BUFFER_SET_MODE",
+            "BUFFER",
+            self.name,
             self.cmd_BUFFER_SET_MODE,
-            desc="Override buffer motor mode: MODE=FORWARD|BACK|STOP|AUTO")
+            desc="Override buffer motor mode: MODE=FORWARD|BACK|STOP|AUTO",
+        )
         gcode.register_mux_command(
-            'BUFFER_QUERY', 'BUFFER', self.name,
+            "BUFFER_QUERY",
+            "BUFFER",
+            self.name,
             self.cmd_BUFFER_QUERY,
-            desc="Report last known buffer sensor/motor state")
+            desc="Report last known buffer sensor/motor state",
+        )
 
     def _handle_connect(self):
         self.set_mode_cmd = self.mcu.lookup_command(
-            'buffer_set_mode mode=%c timeout=%u')
-        self.query_cmd = self.mcu.lookup_command('buffer_query_state')
-        state_format = ('buffer_state hall1=%c hall2=%c hall3=%c fil=%c '
-                        'error=%c state=%c host_mode=%c')
-        if hasattr(self.mcu, 'register_serial_response'):
+            "buffer_set_mode mode=%c timeout=%u"
+        )
+        self.query_cmd = self.mcu.lookup_command("buffer_query_state")
+        state_format = (
+            "buffer_state hall1=%c hall2=%c hall3=%c fil=%c "
+            "error=%c state=%c host_mode=%c"
+        )
+        if hasattr(self.mcu, "register_serial_response"):
             self.mcu.register_serial_response(self._handle_state, state_format)
         else:
             self.mcu.register_response(self._handle_state, state_format)
@@ -92,24 +117,27 @@ class BufferManager:
 
     def _handle_state(self, params):
         eventtime = self.reactor.monotonic()
-        for num, key in ((1, 'hall1'), (2, 'hall2'), (3, 'hall3')):
+        for num, key in ((1, "hall1"), (2, "hall2"), (3, "hall3")):
             if params[key]:
                 if self.hall_stuck_since[num] is None:
                     self.hall_stuck_since[num] = eventtime
             else:
                 self.hall_stuck_since[num] = None
         self.state = {
-            'hall1': params['hall1'], 'hall2': params['hall2'],
-            'hall3': params['hall3'], 'fil': params['fil'],
-            'error': params['error'], 'motor_state': params['state'],
-            'host_mode': params['host_mode'],
+            "hall1": params["hall1"],
+            "hall2": params["hall2"],
+            "hall3": params["hall3"],
+            "fil": params["fil"],
+            "error": params["error"],
+            "motor_state": params["state"],
+            "host_mode": params["host_mode"],
         }
         self.last_state_t = eventtime
 
     def hall_stuck_seconds(self, hall_num, eventtime=None):
         since = self.hall_stuck_since.get(hall_num)
         if since is None:
-            return 0.
+            return 0.0
         if eventtime is None:
             eventtime = self.reactor.monotonic()
         return eventtime - since
@@ -118,14 +146,16 @@ class BufferManager:
         if self.query_cmd is not None:
             self.query_cmd.send()
         return eventtime + QUERY_INTERVAL
-    
+
     def get_status(self, eventtime):
         status = dict(self.state)
-        status['host_mode_active'] = self.host_mode != MODE_AUTO
-        status['stall_seconds'] = self.stall_seconds
-        status['hall1_stuck_s'] = self.hall_stuck_seconds(1, eventtime)
-        status['hall2_stuck_s'] = self.hall_stuck_seconds(2, eventtime)
-        status['hall3_stuck_s'] = self.hall_stuck_seconds(3, eventtime)
+        status["tool"] = self.tool
+        status["mcu"] = 'mcu ' + self.mcu_name
+        status["host_mode_active"] = self.host_mode != MODE_AUTO
+        status["stall_seconds"] = self.stall_seconds
+        status["hall1_stuck_s"] = self.hall_stuck_seconds(1, eventtime)
+        status["hall2_stuck_s"] = self.hall_stuck_seconds(2, eventtime)
+        status["hall3_stuck_s"] = self.hall_stuck_seconds(3, eventtime)
         return status
 
     # ---------------- mode control ----------------
@@ -149,15 +179,17 @@ class BufferManager:
         if mode == MODE_AUTO:
             self.reactor.update_timer(self.keepalive_timer, self.reactor.NEVER)
         else:
-            self.reactor.update_timer(self.keepalive_timer,
-                                       eventtime + KEEPALIVE_INTERVAL)
+            self.reactor.update_timer(
+                self.keepalive_timer, eventtime + KEEPALIVE_INTERVAL
+            )
 
     def cmd_BUFFER_SET_MODE(self, gcmd):
-        mode_str = gcmd.get('MODE').upper()
+        mode_str = gcmd.get("MODE").upper()
         if mode_str not in MODE_NAMES:
             raise gcmd.error(
                 "Unknown MODE '%s', expected one of %s"
-                % (mode_str, ', '.join(MODE_NAMES)))
+                % (mode_str, ", ".join(MODE_NAMES))
+            )
         self.set_mode(MODE_NAMES[mode_str])
         gcmd.respond_info("Buffer %s mode set to %s" % (self.name, mode_str))
 
@@ -168,9 +200,18 @@ class BufferManager:
         gcmd.respond_info(
             "Buffer %s: hall1=%d hall2=%d hall3=%d fil=%d error=%d "
             "motor_state=%d host_mode=%d age=%s"
-            % (self.name, self.state['hall1'], self.state['hall2'],
-               self.state['hall3'], self.state['fil'], self.state['error'],
-               self.state['motor_state'], self.state['host_mode'], age))
+            % (
+                self.name,
+                self.state["hall1"],
+                self.state["hall2"],
+                self.state["hall3"],
+                self.state["fil"],
+                self.state["error"],
+                self.state["motor_state"],
+                self.state["host_mode"],
+                age,
+            )
+        )
 
     # ------------- position_sources interface (for filament_watcher) -------------
 
@@ -188,11 +229,6 @@ class BufferManager:
 
     def get_direction(self):
         return self.direction
-
-    def get_status(self, eventtime):
-        status = dict(self.state)
-        status['host_mode_active'] = self.host_mode != MODE_AUTO
-        return status
 
 
 def load_config_prefix(config):
